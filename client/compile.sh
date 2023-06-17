9a9db60ea7454cd74f0c8e6e2f597dc164f2e318
Blayung <gpewojan1@gmail.com>
2023-06-17 18:57:55 +0200

    add new client

--- client/compile.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-source /usr/lib/emsdk/emsdk_env.sh
-rm -rf ../docs
-cargo web deploy --release --use-system-emscripten --output ../docs
--- client/src/main.rs
+++ client/src/main.rs
@@ -1,34 +1,66 @@
-extern crate sdl2;
+mod game_state;
 
-mod emscripten_boilerplate;
-
-use emscripten_boilerplate::setup_mainloop;
-use sdl2::pixels::Color;
-use sdl2::rect::Rect;
+const FPS_LIMIT: u32 = 60;
+const SCREEN_HEIGHT: u32 = 500;
+const SCREEN_WIDTH: u32 = 500;
+const TICKRATE: u32 = 20;
+const SERVER_IP: &str = "http://localhost:8888";
 
 pub fn main() {
+    // Normal vars
+    //
+
+    // SDL2 Vars
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
-    let window = video_subsystem
-        .window("Kum.io", 450, 450)
-        .build()
-        .unwrap();
+    let window = video_subsystem.window("Kum.io client", SCREEN_WIDTH, SCREEN_HEIGHT).position_centered().opengl().build().unwrap();
     let mut canvas = window.into_canvas().build().unwrap();
+    let mut event_pump = sdl_context.event_pump().unwrap();
+    
+    // Every-tick thread
+    std::thread::spawn(move || {
+        let mut tick_start: std::time::Instant;
+        loop {
+            tick_start = std::time::Instant::now();
+        
+            game_state::set(serde_json::from_str(&reqwest::blocking::get(SERVER_IP.to_string()+"/get_game_state").unwrap().text().unwrap()).unwrap());
+        
+            std::thread::sleep(std::time::Duration::new(0, 1_000_000_000u32 / TICKRATE).checked_sub(tick_start.elapsed()).unwrap_or(std::time::Duration::ZERO));
+        }
+    });
 
-    let fps = -1; // call the function as fast as the browser wants to render (typically 60fps)
-    let simulate_infinite_loop = 1; // call the function repeatedly
-    let mut iteration = 0;
-    setup_mainloop(fps, simulate_infinite_loop, move || {
-        // example: draw a moving rectangle
+    // Main loop
+    let mut frame_start: std::time::Instant;
+    'main_loop: loop {
+        frame_start = std::time::Instant::now();
 
-        // red background
-        canvas.set_draw_color(Color::RGB(255, 0, 0));
+        // Events
+        for event in event_pump.poll_iter() {
+            match event {
+                sdl2::event::Event::Quit {..} | sdl2::event::Event::KeyDown { keycode: Some(sdl2::keyboard::Keycode::Escape), .. } => break 'main_loop,
+                sdl2::event::Event::KeyDown { keycode: Some(sdl2::keyboard::Keycode::W), .. } | sdl2::event::Event::KeyDown { keycode: Some(sdl2::keyboard::Keycode::Up), .. } => {},
+                sdl2::event::Event::KeyDown { keycode: Some(sdl2::keyboard::Keycode::D), .. } | sdl2::event::Event::KeyDown { keycode: Some(sdl2::keyboard::Keycode::Right), .. } => {},
+                sdl2::event::Event::KeyDown { keycode: Some(sdl2::keyboard::Keycode::S), .. } | sdl2::event::Event::KeyDown { keycode: Some(sdl2::keyboard::Keycode::Down), .. } => {},
+                sdl2::event::Event::KeyDown { keycode: Some(sdl2::keyboard::Keycode::A), .. } | sdl2::event::Event::KeyDown { keycode: Some(sdl2::keyboard::Keycode::Left), .. } => {},
+                _ => {}
+            }
+        }
+
+        // Every-frame stuff
+        println!("{:#?}", game_state::get());
+
+        // Drawing to the screen
+        canvas.set_draw_color(sdl2::pixels::Color::RGB(0, 0, 0));
         canvas.clear();
 
-        // moving blue rectangle
-        iteration = (iteration + 1) % 255;
-        canvas.set_draw_color(Color::RGB(0, 0, 255));
-        let rect = Rect::new(iteration, 50, 50, 50);
-        let _ = canvas.fill_rect(rect);
-    })
+        //canvas.set_draw_color(sdl2::pixels::Color::RGB(255, 255, 255));
+        //for player in &players {
+        //    canvas.fill_rect(sdl2::rect::Rect::new((snake_segment.0 as i16 * cell_size as i16) as i32, (snake_segment.1 as i16 * cell_size as i16) as i32, cell_size as u32, cell_size as u32)).unwrap();
+        //}
+
+        canvas.present();
+
+        // FPS Limit
+        std::thread::sleep(std::time::Duration::new(0, 1_000_000_000u32 / FPS_LIMIT).checked_sub(frame_start.elapsed()).unwrap_or(std::time::Duration::ZERO));
+    }
 }
