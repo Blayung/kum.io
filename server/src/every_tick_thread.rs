4c1161f3cbb0615a6725bbf216289b452126328c
Blayung <gpewojan1@gmail.com>
2023-07-26 21:02:11 +0200

    idk, improved sth

--- server/src/every_tick_thread.rs
+++ server/src/every_tick_thread.rs
@@ -11,21 +11,21 @@ macro_rules! spawn {
                 let mut _game_state=game_state::get();
 
                 // Looping over players
-                let mut index=0;
+                let mut player=0;
                 loop {
-                    if index >= _game_state.players.len() {
+                    if player >= _game_state.players.len() {
                         break;
                     }
+                    player += 1;
 
-                    if _game_state.players[index].last_keep_alive.elapsed().as_secs() > 20 {
+                    if _game_state.players[player].last_keep_alive.elapsed().as_secs() > 20 {
                         // Kicking players when no keep alive packets are sent
-                        logging::_info(format!("We have stopped recieving communications from player \"{}\"! Disconnecting...", _game_state.players[index].nick));
-                        _game_state.players.remove(index);
+                        logging::_info(format!("We have stopped recieving communications from player \"{}\"! Disconnecting...", _game_state.players[player].nick));
+                        _game_state.players.remove(player);
                     } else {
                         // MOVEMENT
-                        let next_move_direction = _game_state.players[index].next_move_direction;
+                        let next_move_direction = _game_state.players[player].next_move_direction;
                         if next_move_direction.is_some() {
-
                             //
                             // To calculate the diagonal values (im not used to such kind of math):
                             //
@@ -35,56 +35,54 @@ macro_rules! spawn {
                             //
                             
                             match next_move_direction.unwrap() {
-                                0 => _game_state.players[index].x += 7.0,
+                                0 => _game_state.players[player].x += 7.0,
                                 1 => {
-                                    _game_state.players[index].x += 4.94974746831;
-                                    _game_state.players[index].y += 4.94974746831;
+                                    _game_state.players[player].x += 4.94974746831;
+                                    _game_state.players[player].y += 4.94974746831;
                                 },
-                                2 => _game_state.players[index].y += 7.0,
+                                2 => _game_state.players[player].y += 7.0,
                                 3 => {
-                                    _game_state.players[index].x -= 4.94974746831;
-                                    _game_state.players[index].y += 4.94974746831;
+                                    _game_state.players[player].x -= 4.94974746831;
+                                    _game_state.players[player].y += 4.94974746831;
                                 },
-                                4 => _game_state.players[index].x -= 7.0,
+                                4 => _game_state.players[player].x -= 7.0,
                                 5 => {
-                                    _game_state.players[index].x -= 4.94974746831;
-                                    _game_state.players[index].y -= 4.94974746831;
+                                    _game_state.players[player].x -= 4.94974746831;
+                                    _game_state.players[player].y -= 4.94974746831;
                                 },
-                                6 => _game_state.players[index].y -= 7.0,
+                                6 => _game_state.players[player].y -= 7.0,
                                 7 => {
-                                    _game_state.players[index].x += 4.94974746831;
-                                    _game_state.players[index].y -= 4.94974746831;
+                                    _game_state.players[player].x += 4.94974746831;
+                                    _game_state.players[player].y -= 4.94974746831;
                                 },
 
-                                8 => _game_state.players[index].x += 14.0,
+                                8 => _game_state.players[player].x += 14.0,
                                 9 => {
-                                    _game_state.players[index].x += 9.89949493661;
-                                    _game_state.players[index].y += 9.89949493661;
+                                    _game_state.players[player].x += 9.89949493661;
+                                    _game_state.players[player].y += 9.89949493661;
                                 },
-                                10 => _game_state.players[index].y += 14.0,
+                                10 => _game_state.players[player].y += 14.0,
                                 11 => {
-                                    _game_state.players[index].x -= 9.89949493661;
-                                    _game_state.players[index].y += 9.89949493661;
+                                    _game_state.players[player].x -= 9.89949493661;
+                                    _game_state.players[player].y += 9.89949493661;
                                 },
-                                12 => _game_state.players[index].x -= 14.0,
+                                12 => _game_state.players[player].x -= 14.0,
                                 13 => {
-                                    _game_state.players[index].x -= 9.89949493661;
-                                    _game_state.players[index].y -= 9.89949493661;
+                                    _game_state.players[player].x -= 9.89949493661;
+                                    _game_state.players[player].y -= 9.89949493661;
                                 },
-                                14 => _game_state.players[index].y -= 14.0,
+                                14 => _game_state.players[player].y -= 14.0,
                                 15 => {
-                                    _game_state.players[index].x += 9.89949493661;
-                                    _game_state.players[index].y -= 9.89949493661;
+                                    _game_state.players[player].x += 9.89949493661;
+                                    _game_state.players[player].y -= 9.89949493661;
                                 },
 
                                 _ => {}
                             }
 
-                            _game_state.players[index].next_move_direction = None;
+                            _game_state.players[player].next_move_direction = None;
                         }
                     }
-
-                    index+=1;
                 }
 
                 // Setting the game state
