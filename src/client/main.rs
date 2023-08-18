00341d04483fd604c77be5692a615fc6dcc8bed3
Blayung <gpewojan1@gmail.com>
2023-08-18 09:56:24 +0200

    now the client assets (textures and fonts) are embedded into the executable.

--- src/client/main.rs
+++ src/client/main.rs
@@ -11,7 +11,7 @@ fn main() {
     // SDL
     let sdl_context = sdl2::init().unwrap();
     let sdl_ttf_context = sdl2::ttf::init().unwrap();
-    let sdl_ttf_font = sdl_ttf_context.load_font(std::path::Path::new("./assets/fonts/MonospaceBold.ttf"), 128).unwrap();
+    let sdl_ttf_font = sdl_ttf_context.load_font_from_rwops(sdl2::rwops::RWops::from_bytes(std::include_bytes!("assets/fonts/MonospaceBold.ttf")).unwrap(), 128).unwrap();
     let video_subsystem = sdl_context.video().unwrap();
     let mut canvas = video_subsystem.window("Kum.io client", 1280, 720).position_centered().opengl().build().unwrap().into_canvas().build().unwrap();
     canvas.set_blend_mode(sdl2::render::BlendMode::Blend);
@@ -22,20 +22,14 @@ fn main() {
     data::http_client::init();
 
     // Textures
-    let player_texture = texture_creator.load_texture(std::path::Path::new("./assets/textures/player.png")).unwrap();
-    let grass_texture = texture_creator.load_texture(std::path::Path::new("./assets/textures/grass.png")).unwrap();
+    let player_texture = texture_creator.load_texture_bytes(std::include_bytes!("assets/textures/player.png")).unwrap();
+    let grass_texture = texture_creator.load_texture_bytes(std::include_bytes!("assets/textures/grass.png")).unwrap();
 
     let ver_info_texture = texture_creator.create_texture_from_surface(sdl_ttf_font.render("Kum.io - Dev build 26/07/2023 23:18").blended(sdl2::pixels::Color::RGB(255,255,255)).unwrap()).unwrap();
 
     let server_conn_err_texture = texture_creator.create_texture_from_surface(sdl_ttf_font.render("Couldn't connect to server!").blended(sdl2::pixels::Color::RGB(255,0,0)).unwrap()).unwrap();
     let invalid_ip_texture = texture_creator.create_texture_from_surface(sdl_ttf_font.render("Invalid IP!").blended(sdl2::pixels::Color::RGB(255,0,0)).unwrap()).unwrap();
     let nick_taken_texture = texture_creator.create_texture_from_surface(sdl_ttf_font.render("This nick is already taken!").blended(sdl2::pixels::Color::RGB(255,0,0)).unwrap()).unwrap();
-
-    // These comments are here cause my 16yo laptop cannot handle textures bigger than 2048x2048 :(
-    //let server_conn_err_texture = texture_creator.create_texture_from_surface(sdl_ttf_font.render("C").blended(sdl2::pixels::Color::RGB(255,0,0)).unwrap()).unwrap();
-    //let invalid_ip_texture = texture_creator.create_texture_from_surface(sdl_ttf_font.render("I").blended(sdl2::pixels::Color::RGB(255,0,0)).unwrap()).unwrap();
-    //let nick_taken_texture = texture_creator.create_texture_from_surface(sdl_ttf_font.render("T").blended(sdl2::pixels::Color::RGB(255,0,0)).unwrap()).unwrap();
-    //let ver_info_texture = texture_creator.create_texture_from_surface(sdl_ttf_font.render("K").blended(sdl2::pixels::Color::RGB(255,255,255)).unwrap()).unwrap();
 
     // Other
     let mut game_stage: u8 = 0;
